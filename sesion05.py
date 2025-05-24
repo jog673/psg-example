6cff99102baf178ab250c6a7916d877e3a291dd4
Jorge Maldonado <jog6731997@gmail.com>
2025-05-24 14:40:44 -0400

    Clase sesion05

---
+++ sesion05.py
@@ -0,0 +1,57 @@
+print(type(3.2))
+variable_3= 2.0e-3
+print(variable_3)
+
+#operaciones más complejas
+minutos = 300
+tiempo_extra_segundos = 3600
+horas = (minutos + tiempo_extra_segundos /60)/60
+print(horas)
+
+#operadores aritmeticos
+# Operadores aritméticos
+a = 10
+b = 3
+# Suma
+print ("Suma")
+print (a + b)
+# Resta
+print ("Resta")
+print (a - b)
+# Multiplicación
+print ("Multiplicación")
+print (a * b)
+# División
+print ("División")
+print (a / b)
+# Potencia
+print ("Potencia")
+print (a ** b)
+# Módulo o residuo
+print ("Módulo o residuo")
+print (a % b)
+# División entera
+print ("División entera")
+print (a // b)
+
+#operadores de comparación
+print ("Operadores de comparación")
+comparar = 10
+print (comparar < 10)
+print (comparar > 10)
+print (comparar == 10)
+print (comparar <= 10)
+print (comparar >= 10)
+print (comparar != 10)
+
+
+#símbolos
+print ("Operadores de comparación con int - float")
+entero = 10
+flotante = 10.0
+print (entero < flotante)
+print (entero > flotante)
+print (entero == flotante)
+print (entero <= flotante)
+print (entero >= flotante)
+print (entero != flotante)
