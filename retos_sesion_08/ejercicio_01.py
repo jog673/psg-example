eac407ad9bf389a701f2d0e3bb6689d075d273b8
Jorge Maldonado <jog6731997@gmail.com>
2025-06-14 13:31:51 -0400

    Ejercicio 1

---
+++ retos_sesion_08/ejercicio_01.py
@@ -0,0 +1,7 @@
+# Ingresa por teclado tres coordenadas 'x','y','z',
+#Convierte los valores a enteros almacena los 3 valores en una tupla e imprime el resultado
+variable_1=int(input("Ingresar la coordenada X: "))
+variable_2=int(input("Ingresar la coordenada Y: "))
+variable_3=int(input("Ingresar la coordenada Z: "))
+tupla_1=(variable_1,variable_2,variable_3)
+print(tupla_1)
