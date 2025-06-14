eb82e751c36efdf20dce5283316e2e0edee077f5
Jorge Maldonado <jog6731997@gmail.com>
2025-06-14 13:33:18 -0400

    Ejercicio 5

---
+++ retos_sesion_08/ejercicio_05.py
@@ -0,0 +1,13 @@
+#De la siguiente guía telefónica
+#- Jhon, 591123456, Villa fátima
+#- Jane, 591546324, Obrajes
+#- Dogg, 591222421, Miraflores
+#- Kity, 591753159, Achumani
+#Genera una tupla anidada e imprime el resultado
+tupla_1 = ("Jhon",591123456, "Villa Fatima")
+tupla_2 = ("Jane",591546324, "Obrajes")
+tupla_3 = ("Jhon",591222421, "Miraflores")
+tupla_4 = ("Jhon",591753159, "Achumani")
+tupla_anidada=(tupla_1,tupla_2,tupla_3,tupla_4)
+print(tupla_anidada)
+
