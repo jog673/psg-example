2c221a0f53df44934435d50bda3d64c18055293d
Jorge Maldonado <jog6731997@gmail.com>
2025-06-14 13:32:21 -0400

    Ejercicio 2

---
+++ retos_sesion_08/ejercicio_02.py
@@ -0,0 +1,9 @@
+# Crear un tupla con los siguiente elementos
+tupla_1=('a','b','c','d','e','f','g','h','i','j')
+print(tupla_1)#borrar
+print(tupla_1[0])
+print(tupla_1[9])
+print(tupla_1[3:5])
+print(tupla_1[5:9:3])
+print(tupla_1[9:0:-2])
+
