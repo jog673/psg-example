7ffed8db9356f36c6d80bd39fd24f99bb4531e50
Jorge Maldonado <jog6731997@gmail.com>
2025-06-14 13:33:39 -0400

    Ejercicio 6

---
+++ retos_sesion_08/ejercicio_06.py
@@ -0,0 +1,10 @@
+#De las tuplas de coordenadas
+#(x1,y1)=(-20,-20) y (x2,y2)=(40,20)
+#Encuentra las coordenadas del punto medio, almacena en una tupla e imprime el resultado
+tupla_1 = (-20, -20)
+tupla_2 = (40, 20)
+a = (tupla_1[0] + tupla_2[0])/2
+b = (tupla_1[1] + tupla_2[1])/2
+punto_medio = (a,b)
+print(punto_medio)
+
