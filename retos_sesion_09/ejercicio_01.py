0b39763cef2be89e291dedbe9d7965545eeee2dd
Jorge Maldonado <jog6731997@gmail.com>
2025-06-28 14:59:42 -0400

    Ejercicio 1

---
+++ retos_sesion_09/ejercicio_01.py
@@ -0,0 +1,13 @@
+#Crear una lista con 5 números flotantes y 5 enteros y mostrar todos los elementos y sus tipos de datos utilizando sus índices
+lista_A=[1.2,2.4,4.8,5.1,6.9,10,13,15,17,19]
+print(lista_A)
+print(lista_A[0],type(lista_A[0]))
+print(lista_A[1],type(lista_A[1]))
+print(lista_A[2],type(lista_A[2]))
+print(lista_A[3],type(lista_A[3]))
+print(lista_A[4],type(lista_A[4]))
+print(lista_A[5],type(lista_A[5]))
+print(lista_A[6],type(lista_A[6]))
+print(lista_A[7],type(lista_A[7]))
+print(lista_A[8],type(lista_A[8]))
+print(lista_A[9],type(lista_A[9]))
