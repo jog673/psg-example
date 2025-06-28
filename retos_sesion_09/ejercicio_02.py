c76740ad6d8f09c87975520f5eb37f5706127c00
Jorge Maldonado <jog6731997@gmail.com>
2025-06-28 15:00:10 -0400

    Ejercicio 2

---
+++ retos_sesion_09/ejercicio_02.py
@@ -0,0 +1,5 @@
+#De la siguiente lista [5,4,3,2,2,2,0,0,1,2] obtener una sub lista inversa utilizando saltos de 3 en 3
+lista=[5,4,3,2,2,2,0,0,1,2]
+print(lista)
+copia = lista[-1:-10:-3]
+print (copia)
