ed34aed051d96c9008e9cdf39127fee9e100bddc
Jorge Maldonado <jog6731997@gmail.com>
2025-07-05 15:02:43 -0400

    ejercicio_02

---
+++ retos_sesion_10/ejercicio_02.py
@@ -0,0 +1,5 @@
+#El dueño de una tienda de ropa deportiva ha comprado ropa formal y quiere abrir una nueva tienda que combine ambos estilos. Crea un conjunto con las prendas de ambos tipos con las listas de prendas
+tienda_1=["Short", "Playera", "Sudadera", "Tenis", "Short", "Calcetines"]
+tienda_2=["Saco", "Corbata", "Pantalón de vestir", "Zapatos", "Calcetines"]
+conjunto = set(tienda_1+tienda_2)
+print(conjunto)
