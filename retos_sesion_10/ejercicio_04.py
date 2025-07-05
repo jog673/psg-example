075f6a82af6706b394aec7b34b7746e9d994b4eb
Jorge Maldonado <jog6731997@gmail.com>
2025-07-05 15:04:10 -0400

    ejercicio_04

---
+++ retos_sesion_10/ejercicio_04.py
@@ -0,0 +1,11 @@
+#Jane y Jhon llevan saliendo juntos por 4 semanas, cada vez que salen van a comer a un candy bar. 
+# Quieren saber que tan compatibles son viendo cuantos platos de comida tienen en común. 
+# A continuación tienes los postres que han ido pidiendo en cada salida:
+Jane = {'Lemon Pie', 'Brownie', 'Tarta de manzana', 'Helado de chocolate', 'Flan'}
+Jhon = {'Carrot Cake', 'Croissant de chocolate', 'Lemon Pie', 'Tarta de manzana', 'Pudding'}
+interseccion = Jane.intersection(Jhon)
+a=len(Jane)
+print(interseccion)
+b=len(interseccion)
+print(b >= a/2)
+
