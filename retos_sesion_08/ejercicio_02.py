1176ac8660c1541d45756e905c956678a1552e76
Jorge Maldonado <jog6731997@gmail.com>
2025-07-12 19:45:41 -0400

    ejercicio 2 corregido

--- retos_sesion_08/ejercicio_02.py
+++ retos_sesion_08/ejercicio_02.py
@@ -3,7 +3,7 @@ tupla_1=('a','b','c','d','e','f','g','h','i','j')
 print(tupla_1)#borrar
 print(tupla_1[0])
 print(tupla_1[9])
-print(tupla_1[3:5])
-print(tupla_1[5:9:3])
-print(tupla_1[9:0:-2])
+print(tupla_1[3:6])
+print(tupla_1[5:10:3])
+print(tupla_1[9::-2])
 
