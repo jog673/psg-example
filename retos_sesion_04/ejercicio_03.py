fbf4c40b884705e2af7f27f8171ddb703713769a
Jorge Maldonado <jog6731997@gmail.com>
2025-07-12 23:11:05 -0400

    ejercicio03 corregido

--- retos_sesion_04/ejercicio_03.py
+++ retos_sesion_04/ejercicio_03.py
@@ -1,10 +1,42 @@
+print("🧠 Juego de mesa: Ajedrez")
+print("")
+
+print("📏Tablero")
 print("Tablero: 8x8 casillas, alternando colores claros y oscuros")
-print("Piezas: Cada jugador inicia con 16 (rey, dama, 2 torres, 2 alfiles, 2 caballos, 8 peones).")
-print("Objetivo: Dar jaque mate (rey atacado sin escapatoria).")
-print("Movimientos: Peón: Avanza recto, captura en diagonal. En salida, puede avanzar 2 casillas.Torre: Horizontal/vertical.Caballo: En ele (2+1 casillas).Alfil: Diagonal.Dama: Combina torre y alfil.Rey: 1 casilla en cualquier dirección.")
+
+print("♟️ Piezas:")
+print(" Cada jugador inicia con 16 piezas")
+print("- 1 rey")
+print("- 1 dama")
+print("- 2 torres")
+print("- 2 alfiles")
+print("- 2 caballos")
+print("- 8 peones")
+
+print("🎯 Objetivos")
+print("Dar jaque mate (rey atacado sin escapatoria).")
+
+
+print("↕️ Movimientos:")
+print("Peón: Avanza recto, captura en diagonal.En salida, puede avanzar 2 casillas")
+print("Torre: Horizontal/vertical")
+print("Caballo: En ele (2+1 casillas)")
+print("Alfil: Diagonal")
+print("Dama: Combina torre y alfil")
+print("Rey: 1 casilla en cualquier dirección")
+
+
+print("🔩 Reglas especiales:")
 print("Enroque: Mueve rey y torre juntos (protección). Solo si no han movido antes")
 print("Captura al paso: Peón captura otro peón que avanzó 2 casillas, como si hubiera movido 1.")
 print("Promoción: Peón que llega al extremo se convierte en dama, torre, alfil o caballo.")
-print("Jaque: Rey amenazado. Debe evitarse (mover, bloquear o capturar)")
-print("Jaque mate: Rey en jaque sin defensa. Fin de la partida (victoria).")
-print("Tablas: Por ahogado (rey sin movimientos legales y no en jaque), repetición de jugadas, acuerdo o insuficiencia material.")
+
+
+print("🛡️ Jaque:")
+print("Rey amenazado. Debe evitarse (mover, bloquear o capturar)")
+
+print("☠️ Jaque mate:")
+print("El rey esta amenazado sin defensa. Fin de la partida (victoria).")
+
+print("🤝 Tablas(empate):")
+print("Ocurre por ahogo del rey (sin movimientos legales y no en jaque), repetición de jugadas, acuerdo o insuficiencia material.")
