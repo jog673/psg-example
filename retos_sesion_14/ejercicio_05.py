be64fd1a24cafdea624f2846c7ae2e1a0c3368cb
Jorge Maldonado <jog6731997@gmail.com>
2025-08-02 15:19:41 -0400

    ejercicio_05

---
+++ retos_sesion_14/ejercicio_05.py
@@ -0,0 +1,9 @@
+# Crear una función que reciba una cadena y devuelva la cantidad de vocales que tiene
+def cantidad_vocales(cadena):
+    cadena = cadena.title()
+    vocales = sum([1 for letra in cadena if letra in "aeiou"])
+    return vocales
+cadena = "BOLIVIA"
+resultado = cantidad_vocales(cadena)
+
+print(resultado)
