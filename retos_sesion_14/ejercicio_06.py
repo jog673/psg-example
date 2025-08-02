b9cba12e3c0c5db0f1a93a69d6fbb88b4cc62944
Jorge Maldonado <jog6731997@gmail.com>
2025-08-02 15:19:59 -0400

    ejercicio_06

---
+++ retos_sesion_14/ejercicio_06.py
@@ -0,0 +1,15 @@
+def separar_pares_impares(lista):
+    pares = []
+    impares = []
+    for numero in lista:
+        if numero %2 == 0:
+            pares.append(numero)
+        else:
+            impares.append(numero)
+    return pares, impares
+numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+pares,impares = separar_pares_impares(numeros)
+
+print("pares", pares)
+print("impares", impares)
