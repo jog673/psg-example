5dd2e0589c23818a5f96c4c599bc72f886d4130a
Jorge Maldonado <jog6731997@gmail.com>
2025-08-16 11:27:00 -0400

    Corrección retos sesión13

--- retos_sesion_13/ejercicio_04.py
+++ retos_sesion_13/ejercicio_04.py
@@ -1,10 +1,10 @@
 while True:
     frase = input("Ingresar una frase:  ")
+    if "salir" in frase.lower():
+        break
     frase = "".join(filter(str.isalnum,frase)).lower()
     palindromo = ((frase==frase[::-1]))
     print(f"La frase es palindromo: {palindromo}")
-    if frase == 'salir':
-        break
 print("Fin")
 
 
